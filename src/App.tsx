1905e472f90f52743dc618d064c9ac74b0047848
Katy Pacheco <katypacheco@ravn.co>
2025-05-26 18:03:49 -0600

    fix: resolve merge conflict in main.tsx and clean up render method

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
-
-function App() {
-  return <></>;
-=======
 import CharacterPage from './components/CharacterPage/character-page';
 
 function App() {
@@ -12,7 +7,6 @@ function App() {
       <CharacterPage />
     </>
   );
->>>>>>> 0e5add1a8a8833ce356ea2d2a844a1d900798fe8
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-<<<<<<< HEAD
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-=======
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
@@ -22,5 +15,4 @@ createRoot(document.getElementById('root')!).render(
       <App />
     </StrictMode>
   </ApolloProvider>,
->>>>>>> 0e5add1a8a8833ce356ea2d2a844a1d900798fe8
 );
